245f5f613b23b445b526069e3a33c46374fcb0cb
x7md <me@x7md.net>
2025-02-12 17:29:45 +0300

    fix it

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,31 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [
-          tailwindcss()
-        ],
-      },
+  site: 'https://athar-org-v2.pages.dev',
+  vite: {
+      plugins: [
+        tailwindcss()
+      ],
+    },
+    i18n: {
+      locales: ["ar","en"],
+      defaultLocale: "ar",
+    },
+  integrations: [
+    sitemap(
+    {
+      i18n: {
+        defaultLocale: 'ar', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          ar: 'ar-SA',
+          en: 'en-US',
+        }
+      }
+    }
+  )],
 });
